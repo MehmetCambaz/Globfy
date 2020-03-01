d04ba59d7eeb9f47b27a0135cb2e7aed0b02b663
MehmetCambaz <52370708+MehmetCambaz@users.noreply.github.com>
2020-03-02 02:27:01 +0300

    Add files via upload

--- Globfy-master/denemeEditor/AYARcontent4.php
+++ Globfy-master/denemeEditor/AYARcontent4.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 if(isset($_POST['submit'])){
     $file = $_FILES['content4_resim'];
     
@@ -16,30 +14,42 @@ if(isset($_POST['submit'])){
     $fileActualExt = strtolower(end($fileExt));
 
     $allowed = array('jpg', 'jpeg', 'png');
+    $error = fopen("ErrorLogs.txt", "a");
 
     if(in_array($fileActualExt, $allowed)){
         if($fileError === 0){
-            if($fileSize < 50000){
+            if($fileSize < 999999999999){
                 $fileNameNew = uniqid('', true).".".$fileActualExt;
                 $fileDestination = $fileName;
                 move_uploaded_file($fileTmpName, $fileDestination);
             }else{
                 echo "Resim fazla büyük !";
+                $txt = "Resim fazla büyük !\n";
+                fwrite($error, $txt);
+                fclose($error);
             }
         }else{
             echo "Resim yüklenirken hata oluştu !";
+            $txt = "Resim yüklenirken hata oluştu !\n";
+            fwrite($error, $txt);
+            fclose($error);
         }
     }else{
         echo "Bu türdeki dosyaları yükleyemezsiniz !";
+        $txt = "Bu türdeki dosyaları yükleyemezsiniz !\n";
+        fwrite($error, $txt);
+        fclose($error);
     }
 }
 
 $deger1=$_POST["content4_yazim"];
 $deger2=$_POST["content4_resim"];
 $deger3=$_POST["content4_arkaplan"];
+$deger4=$_POST["content4_yazim_rengi"];
 setcookie("sayfaEkle[content4_yazim]",$deger1,time()+3600);
-setcookie("sayfaEkle[content4_resim]",$deger2,time()+3600);
+setcookie("sayfaEkle[content4_resim]",$fileName,time()+3600);
 setcookie("sayfaEkle[content4_arkaplan]",$deger3,time()+3600);
+setcookie("sayfaEkle[content4_yazim_rengi]",$deger4,time()+3600);
 
 header("Location: index3.php?KompanentEkle=12");
 
